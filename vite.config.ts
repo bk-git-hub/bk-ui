535db9807c10c9d7dbbab509fdf73de06f620e8a
bk-git-hub <bkprivate99@gmail.com>
2025-10-09 16:26:34 +0900

    chore: add hosting option in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "0.0.0.0", // 네트워크 전체에서 접근 가능
+    port: 5173, // 기본 포트 (원하는 걸로 바꿀 수 있음)
+  },
 });
